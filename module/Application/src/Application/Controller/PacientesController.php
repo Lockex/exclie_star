b13154214cfe7bd1c41d1dd716a86ca788be00d4
sz.samuel <samuel.saenz@exclie.com.mx>
2016-02-26 11:54:36 -0700

    Ya funciona todo
    cambio de ID que creaba el errorsote

--- module/Application/src/Application/Controller/PacientesController.php
+++ module/Application/src/Application/Controller/PacientesController.php
@@ -9,10 +9,9 @@
 
 namespace Application\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use Application\Entity\Antecedentes;
 use Application\Entity\Pacientes;
-
+use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\JsonModel;
 use Zend\View\Model\ViewModel;
 
@@ -57,49 +56,47 @@ class PacientesController extends AbstractActionController {
 REGISTRANDO PACIENTE
  ******************************************************************/
 
+	public function guardarpacienteAction() {
+		$om = $this->getObjectManager();
+		if ($this->request->isPost()) {
+			$paciente = new Pacientes();
+			$paciente->setSEXO($this->request->getPost('SEXO'));
+			$paciente->setNOMBRE($this->request->getPost('NOMBRE'));
+			$paciente->setAPELLIDO_PATERNO($this->request->getPost('APELLIDO_PATERNO'));
+			$paciente->setAPELLIDO_MATERNO($this->request->getPost('APELLIDO_MATERNO'));
+			$paciente->setESTADO($om->find('Application\Entity\Estados', $this->request->getPost('ESTADO')));
+			$paciente->setMUNICIPIO($om->find('Application\Entity\Municipios', $this->request->getPost('MUNICIPIO')));
+			$paciente->setCALLE($this->request->getPost('CALLE'));
+			$paciente->setNUMEROINT($this->request->getPost('NUM_INT'));
+			$paciente->setNUMEROEXT($this->request->getPost('NUM_EXT'));
+			$paciente->setTELEFONO1($this->request->getPost('TELEFONO1'));
+			$paciente->setTELEFONO2($this->request->getPost('TELEFONO2'));
+			$paciente->setOCUPACION($this->request->getPost('OCUPACION'));
+			$paciente->setESTADOCIVIL($this->request->getPost('ESTADO_CIVIL'));
+			$paciente->setFECHA_NACIMIENTO(new \DateTime(date('Y-m-d', strtotime($this->request->getPost('FECHA_NACIMIENTO')))));
+			$paciente->setFECHAREGISTRO(new \DateTime());
+			$paciente->setTIPOSANGUINEO($om->find('Application\Entity\Tipossanguineos', $this->request->getPost('TIPO_SANGUINEO')));
 
-    public function guardarpacienteAction()
-    {
-      $om = $this->getObjectManager();
-      if($this->request->isPost()) {
-        $paciente = new Pacientes();
-        $paciente->setSEXO($this->request->getPost('SEXO'));
-        $paciente->setNOMBRE($this->request->getPost('NOMBRE'));
-        $paciente->setAPELLIDO_PATERNO($this->request->getPost('APELLIDO_PATERNO'));
-        $paciente->setAPELLIDO_MATERNO($this->request->getPost('APELLIDO_MATERNO'));
-        $paciente->setESTADO($om->find('Application\Entity\Estados',$this->request->getPost('ESTADO')));
-        $paciente->setMUNICIPIO($om->find('Application\Entity\Municipios',$this->request->getPost('MUNICIPIO')));
-        $paciente->setCALLE($this->request->getPost('CALLE'));
-        $paciente->setNUMEROINT($this->request->getPost('NUM_INT'));
-        $paciente->setNUMEROEXT($this->request->getPost('NUM_EXT'));
-        $paciente->setTELEFONO1($this->request->getPost('TELEFONO1'));
-        $paciente->setTELEFONO2($this->request->getPost('TELEFONO2'));
-        $paciente->setOCUPACION($this->request->getPost('OCUPACION'));
-        $paciente->setESTADOCIVIL($this->request->getPost('ESTADO_CIVIL'));
-        $paciente->setFECHA_NACIMIENTO(new \DateTime(date('Y-m-d',strtotime($this->request->getPost('FECHA_NACIMIENTO')))));
-        $paciente->setFECHAREGISTRO(new \DateTime());
-        $paciente->setTIPOSANGUINEO($om->find('Application\Entity\Tipossanguineos',$this->request->getPost('TIPO_SANGUINEO'
+			$om->persist($paciente);
+			$om->flush();
 
-		$om->persist($paciente);
-		$om->flush();
+			$antecedentes = new Antecedentes();
+			$antecedentes->setPACIENTE($paciente); // Paciente recién registrado.
+			$antecedentes->setTABAQUISMO($this->request->getPost('TABAQUISMO'));
+			$antecedentes->setALCOHOLISMO($this->request->getPost('ALCOHOLISMO'));
+			$antecedentes->setALERGIAS($this->request->getPost('ALERGIAS'));
+			$antecedentes->setTOXICOMANIAS($this->request->getPost('TOXICOMANIAS'));
+			$antecedentes->setCIRUGIAS($this->request->getPost('CIRUGIAS'));
+			$antecedentes->setDIABETES($this->request->getPost('DIABETES'));
+			$antecedentes->setARTRITIS($this->request->getPost('ARTRITIS'));
+			$antecedentes->setCANCER($this->request->getPost('CANCER'));
+			$antecedentes->setLUPUS($this->request->getPost('LUPUS'));
+			$antecedentes->setCARDIOPATIAS($this->request->getPost('CARDIACAS'));
+			$antecedentes->setHIPERTENSION($this->request->getPost('HIPERTENSION'));
+			$antecedentes->setTIROIDES($this->request->getPost('TIROIDES'));
 
-		$antecedentes = new Antecedentes();
-		$antecedentes->setPACIENTE($paciente); // Paciente recién registrado.
-		$antecedentes->setTABAQUISMO($this->request->getPost('TABAQUISMO'));
-		$antecedentes->setALCOHOLISMO($this->request->getPost('ALCOHOLISMO'));
-		$antecedentes->setALERGIAS($this->request->getPost('ALERGIAS'));
-		$antecedentes->setTOXICOMANIAS($this->request->getPost('TOXICOMANIAS'));
-		$antecedentes->setCIRUGIAS($this->request->getPost('CIRUGIAS'));
-		$antecedentes->setDIABETES($this->request->getPost('DIABETES'));
-		$antecedentes->setARTRITIS($this->request->getPost('ARTRITIS'));
-		$antecedentes->setCANCER($this->request->getPost('CANCER'));
-		$antecedentes->setLUPUS($this->request->getPost('LUPUS'));
-		$antecedentes->setCARDIOPATIAS($this->request->getPost('CARDIACAS'));
-		$antecedentes->setHIPERTENSION($this->request->getPost('HIPERTENSION'));
-		$antecedentes->setTIROIDES($this->request->getPost('TIROIDES'));
-
-		$om->persist($antecedentes);
-		$om->flush();
+			$om->persist($antecedentes);
+			$om->flush();
 		}
 		return new JsonModel();
 	}
