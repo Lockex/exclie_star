98984da4886dbc8f9ddd40111f9aba335aa76d00
sz.samuel <samuel.saenz@exclie.com.mx>
2016-03-01 16:33:38 -0700

    commit 1.03.16
    guarda imagenes chidas

--- module/Application/src/Application/Controller/ConsultadosController.php
+++ module/Application/src/Application/Controller/ConsultadosController.php
@@ -459,15 +459,14 @@ class ConsultadosController extends AbstractActionController {
 				$this->getRequest()->getPost()->toArray(),
 				$this->getRequest()->getFiles()->toArray()
 			);
+			$imagen = $data['file']['name'];
 
 			//$fecha_consulta = $data['fecha_consulta'];
 			$videos = new Videoconsulta();
 
-			$videos->setVIDEO($data['file']['name']);
+			$videos->setVIDEO($imagen);
 			$videos->setPACIENTE($objectManager->find('Application\Entity\Pacientes', $data['pacid']));
 			$videos->setFECHA(new \DateTime());
-			$objectManager->persist($videos);
-			$objectManager->flush();
 
 			$ruta = getcwd() . '/public/imagenes/videos/' . $data['pacid'];
 			if (!file_exists($ruta)) {
@@ -475,7 +474,14 @@ class ConsultadosController extends AbstractActionController {
 			}
 			$adapter = new \Zend\File\Transfer\Adapter\Http();
 			$adapter->setDestination($ruta);
+			$adapter->addfilter('Rename', array(
+				'target' => $ruta . "/" . $imagen,
+				'overwrite' => true,
+			));
+
 			if ($adapter->receive()) {
+				$objectManager->persist($videos);
+				$objectManager->flush();
 
 			}
 		}
@@ -485,18 +491,18 @@ class ConsultadosController extends AbstractActionController {
 	public function eliminarVideoAction() {
 		if ($this->request->getPost()) {
 			$om = $this->getObjectManager();
-			$imagen = $this->request->getPost('archivo');
+			$nomvideo = $this->request->getPost('archivo');
 			$idPaciente = $this->request->getPost('id');
 
-			$query = $om->createQuery("SELECT v.ID FROM Application\Entity\Videoconsulta v WHERE v.VIDEOS = '$imagen' AND v.PACIENTE = $idPaciente");
-			$foto = $query->getArrayResult();
+			$query = $om->createQuery("SELECT v.ID FROM Application\Entity\Videoconsulta v WHERE v.VIDEO = '$nomvideo' AND v.PACIENTE = $idPaciente");
+			$vid = $query->getArrayResult();
 
-			$quitar = $om->find('Application\Entity\Videoconsulta', $foto[0]['ID']);
+			$quitar = $om->find('Application\Entity\Videoconsulta', $vid[0]['ID']);
 
 			$om->remove($quitar);
 			$om->flush();
 
-			$ruta = getcwd() . '/public/imagenes/videos/' . $idPaciente . '/' . $imagen;
+			$ruta = getcwd() . '/public/imagenes/videos/' . $idPaciente . '/' . $nomvideo;
 
 			unlink($ruta);
 
